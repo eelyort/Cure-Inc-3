f589cbd6561801685d785d6625e3735e9e7f83d8
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2020-03-28 16:58:54 -0500

    Added GUI (Functional but UGLY)

---
+++ Assets/Scripts/GUI/TextStuff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextStuff : MonoBehaviour
+{
+    //public Text test;
+    public GameObject test2;
+
+    public void Start()
+    {
+        //test = gameObject.GetComponent<Text>();
+        //test.text = "uwu";
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void testText()
+    {
+        //test.text = "Hewwo";
+        test2.GetComponent<Text>().text = "Hewwo";
+    }
+
+}
