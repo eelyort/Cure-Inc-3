6cffab072f732b86c762e6d97629ce64fc1955d3
Pyristix <61607183+Pyristix@users.noreply.github.com>
2020-03-28 16:02:32 -0500

    Asset stuff?

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MainGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+	//Variables
+	LinkedList<int> nodeList = new LinkedList<int>();
+	LinkedListNode<int> firstNode;
+	LinkedListNode<int> currentNode;
+	
+	int difficulty = 1;
+	int enemySpawnRate = 1;
+	int playerSpawnRate = 1;
+	int tickCount = 0;
+	
+	
+	
+	
+	
+	// Start is called before the first frame update
     void Start()
     {
-        
+        //Sets up first node and currentNode variable for iteration
+		firstNode = nodeList.First;
+		currentNode = firstNode;
+		
+		//TODO: Set up variable values depending on difficulty
+		
     }
 
     // Update is called once per frame
     void Update()
     {
         
+		currentNode = currentNode.Next;
+		
     }
 }
