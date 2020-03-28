9defb1b7ebadc59b9d800d00c16e9235a73abd46
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2020-03-28 15:34:39 -0500

    Menu Complete

---
+++ Options.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    int diff = 1;
+
+    public void setEasy()
+    {
+        Debug.Log("Set Easy!");
+        diff = 0;
+    }
+
+    public void setCasual()
+    {
+        Debug.Log("Set Casual!");
+        diff = 1;
+    }
+
+    public void setHard()
+    {
+        Debug.Log("Set Hard!");
+        diff = 2;
+    }
+
+    public int getDiff()
+    {
+        return diff;
+    }
+}
