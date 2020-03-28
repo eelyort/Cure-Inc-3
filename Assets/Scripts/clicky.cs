18bf623e87001a2e766bf12ed7e76a87485bd0c9
vivians-y <55055539+vivians-y@users.noreply.github.com>
2020-03-28 18:58:10 -0500

    color click

---
+++ Assets/Scripts/clicky.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clicky : MonoBehaviour
+{
+        Color[] Data;
+        SpriteRenderer SpriteRenderer;
+    public Color Color;
+
+
+    public int Width { get { return SpriteRenderer.sprite.texture.width; } }
+    public int Height
+    {
+        get { return SpriteRenderer.sprite.texture.height; }
+    }
+
+    void Awake()
+    {
+        // Get renderer you want to probe
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        // extract color data
+        Data = SpriteRenderer.sprite.texture.GetPixels();
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            // Get Mouse position - convert to world position
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            screenPos = new Vector2(screenPos.x, screenPos.y);
+
+            // Check if we clicked on our object
+            RaycastHit2D[] ray = Physics2D.RaycastAll(screenPos, Vector2.zero, 0.01f);
+            for (int i = 0; i < ray.Length; i++)
+            {
+                // You will want to tag the image you want to lookup
+                if (ray[i].collider.tag == "TAGNAME")
+                {
+                    // Set click position to the gameobject area
+                    screenPos -= ray[i].collider.gameObject.transform.position;
+                    int x = (int)(screenPos.x * Width);
+                    int y = (int)(screenPos.y * Height) + Height;
+
+                    // Get color data
+                    if (x > 0 && x < Width && y > 0 && y < Height)
+                    {
+                        Color = Data[y * Width + x];
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
